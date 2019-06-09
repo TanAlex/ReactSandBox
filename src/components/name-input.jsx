9b565ba5f82925f0ecda4c73703be552156cce82
Alex Tan <31178152+TanAlex@users.noreply.github.com>
2019-06-08 22:05:21 -0700

    add debounce

--- src/components/name-input.jsx
+++ src/components/name-input.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 import { HelloContext } from "../context/hello-context.js";
+import _ from "lodash";
+
+//Put the deboncedSetName outside of the function
+//because the stateless function executes every time
+//we have to keep the debounced function out of every run
+
+const debouncedSetName = _.debounce(
+  (setName, newName) => {
+    setName(newName);
+  },
+  400,
+  { leading: true }
+);
 
 export default props => {
   return (
     <HelloContext.Consumer>
-      {({ name, setName }) => (
-        <div class="form-group">
-          <label for="usr">Name:</label>
-          <input
-            type="text"
-            class="form-control"
-            id="usr"
-            onChange={e => {
-              //console.log(e); //don't use e, it will error out, you can use e.target
-              //console.log(name, setName);
-              setName(e.target.value); //use e.target is ok, just don't use e directly
-            }}
-          />
-        </div>
-      )}
+      {({ name, setName }) => {
+        return (
+          <div class="form-group">
+            <label for="usr">Name:</label>
+            <input
+              type="text"
+              class="form-control"
+              id="usr"
+              onChange={e => debouncedSetName(setName, e.target.value)}
+            />
+          </div>
+        );
+      }}
     </HelloContext.Consumer>
   );
 };
